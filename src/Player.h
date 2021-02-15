df7475600032274028e32987f4b034c0d78f9326
TheNeverMan <thenevermanever@gmail.com>
2021-02-15 19:46:36 +0100

    Added game pointer

--- src/Player.h
+++ src/Player.h
@@ -1,6 +1,7 @@
 #ifndef PLAYER_H
 #define PLAYER_H
 #include<string>
+#include "Game.h"
 using namespace std;
 //generic player with virtual stuff
 namespace ChessBattleRoyale
@@ -10,6 +11,7 @@ class Player
 {
 private:
   string Name;
+  Game *gameiamin;
 public:
     Player();
     ~Player();
