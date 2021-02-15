d19f064c38c414c92f2facf501bce3bb05d4a053
TheNeverMan <thenevermanever@gmail.com>
2021-02-15 19:05:28 +0100

    Added Player Name

--- src/Player.h
+++ src/Player.h
@@ -1,11 +1,15 @@
 #ifndef PLAYER_H
 #define PLAYER_H
+#include<string>
+using namespace std;
 //generic player with virtual stuff
 namespace ChessBattleRoyale
 {
 
 class Player
 {
+private:
+  string Name;
 public:
     Player();
     ~Player();
