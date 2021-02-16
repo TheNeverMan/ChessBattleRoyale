15a23b61fb3e6798a5c32fb9ee3ff06d3b88d598
TheNeverMan <thenevermanever@gmail.com>
2021-02-16 14:04:11 +0100

    Added more Game functionalities

--- src/Game.cpp
+++ src/Game.cpp
@@ -2,9 +2,25 @@
 
 ChessBattleRoyale::Game::Game()
 {
+  ZoneLowerX = 0;
+  ZoneLowerY = 8;
+  ZoneUpperX = 8;
+  ZoneUpperY = 0;
+  TurnCounter = 1;
 }
 
 ChessBattleRoyale::Game::~Game()
 {
 }
-
+Piece *ChessBattleRoyale::Game::isEmptyTile(int x,int y)
+{
+  if(ChessBoard[x][y]!=NULL)
+  {
+    return ChessBoard[x][y];
+  }
+  return NULL;
+}
+void ChessBattleRoyale::Game::AddPlayer(Player *playertoadd,Piece *piecetobeadded)
+{
+  
+}
--- src/Game.h
+++ src/Game.h
@@ -1,13 +1,41 @@
 #ifndef GAME_H
 #define GAME_H
+#include<map>
 #include "Player.h"
+#include "Piece.h"
+#include "ChessMap.h"
+#include "Gamemode.h"
+using namespace std;
 //instance of this class is created by frontend from gamemaker and this class coordinates entire game, with players and stuff
 namespace ChessBattleRoyale
 {
 class Player;
+class Piece;
 class Game
 {
+private:
+  map <Player*,Piece*> PlayersMap; // player management
+  Gamemode *gamemode;
+  ChessMap *chessmap; //used map in game
+  Piece *ChessBoard [8][8]; //of course arrays start at 0
+  void LoadMap();
+  int RookDefX;
+  int RookDefY;
+  int KnightDefX;
+  int KnightDefY;
+  int BishopDefX;
+  int BishopDefY;
+  int ZoneUpperX;
+  int ZoneUpperY;
+  int ZoneLowerX;
+  int ZoneLowerY;
+  int TurnCounter;
 public:
+    Piece* isEmptyTile(int x,int y);
+    void TakeTurn();
+    void DeletePlayer(Player * playertoremove);
+    bool isInZone(int x, int y);
+    void AddPlayer(Player *playertoadd,Piece *piecetobeadded); //should load automaticly to start pos
     Game();
     ~Game();
 
