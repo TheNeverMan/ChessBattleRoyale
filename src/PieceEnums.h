744012d36ae9107d565d7ea2ed9be28d0c7efab1
TheNeverMan <thenevermanever@gmail.com>
2021-02-18 14:31:10 +0100

    splited enums from Piece.h to ifferent file

---
+++ src/PieceEnums.h
@@ -0,0 +1,16 @@
+#ifndef INCLUDE_H
+#define INCLUDE_H
+//this is header file made for avoiding include conflicts, it is useless
+using namespace std;
+namespace ChessBattleRoyale
+{
+enum piece_color
+{
+  white,black
+};
+enum whoami
+{
+  Rook,Knight,Bishop,Queen,King,Pawn
+};
+}
+#endif //INCLUDE_H
